c6086a210f8bb2f40e01f3034cfdb089074c18eb
Andrey Tvorozhkov <andrey@head-labs.com>
2024-10-08 16:24:36 +0300

    ?

--- build.py
+++ build.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from dabi.parser import dABIParser
 import os
 
@@ -6,4 +8,4 @@ current_file_path = os.path.dirname(os.path.abspath(__file__))
 if __name__ == "__main__":
     parser = dABIParser(current_file_path)
     result_json = parser.parse()
-    print(result_json)
+    pprint(result_json)
